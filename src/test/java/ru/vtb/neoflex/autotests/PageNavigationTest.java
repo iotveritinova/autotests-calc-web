d803431f76e0f2843bcbedc61dec75eb013c97e3
iotveritinova <51055439+iotveritinova@users.noreply.github.com>
2020-07-09 17:05:23 +0400

    добавлена работа с allure

--- src/test/java/ru/vtb/neoflex/autotests/PageNavigationTest.java
+++ src/test/java/ru/vtb/neoflex/autotests/PageNavigationTest.java
@@ -1,9 +1,8 @@
 package ru.vtb.neoflex.autotests;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,33 +19,54 @@ public class PageNavigationTest {
         Thread.sleep(1000);
     }
 
+    @Step
+    @DisplayName("Navigation test")
+    @Description("Navigation test description")
     @Test
     public void FromMainToSendAndBack() throws InterruptedException {
         MainPage mainPage = new MainPage(driver);
         mainPage.clickSend();
         Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Передача показаний");
-        driver.findElement(By.xpath("//*[@id=\"back_button\"]")).click();
-        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Neo ЖКХ");
-    }
-    @Test
-    public void FromMainToHistoryAndBack () throws InterruptedException {
-        MainPage mainPage = new MainPage(driver);
-        mainPage.clickHistory();
-        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "История показаний");
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"back_button\"]")).click();
         Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Neo ЖКХ");
     }
 
+    @Step
+    @DisplayName("Navigation test2")
+    @Description("Navigation test description2")
     @Test
-    public void FromMainToPriceAndBack () {
+    public void FromMainToHistoryAndBack() throws InterruptedException {
         MainPage mainPage = new MainPage(driver);
-        mainPage.clickPrice();
-        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Справочник стоимости услуг");
+        mainPage.clickHistory();
+        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "История показаний");
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"back_button\"]")).click();
         Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Neo ЖКХ");
     }
+
+    @Step
+    @DisplayName("Navigation test3")
+    @Description("Navigation test description3")
+    @Test
+    public void FromMainToPriceAndBack() throws InterruptedException {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickPrice();
+        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Справочник стоимости услуг");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"back_button\"]")).click();
+        Assertions.assertEquals(driver.findElement(By.xpath("/html/body/h1")).getText(), "Neo ЖКХ");
+    }
+
     @AfterEach
     public void closePage() {
         driver.quit();
     }
+    /*Для того, чтобы после выполнения теста сформировать отчёт и открыть его в браузере, необходимо выполнить следующие действия:
+
+1) Открыть Terminal в IntelliJ IDEA
+
+2) Ввести команду: allure generate --clean
+
+3) Ввести команду: allure serve*/
 }
